1eec15dd510af74c370dfa41136bc7b7d46a0631
lisa-valentina <lisa.riedel@gmail.com>
2022-12-27 18:35:02 +0100

    cleaning up code

--- src/script.js
+++ src/script.js
@@ -1,56 +1,5 @@
-//check final project with dark mode etc. and use this system to change all fahrenheit and celsius, by always changing unit metric to imperial
 //add final css
 //clean up code
-
-function formatTime(timestamp) {
-  let now = new Date(timestamp * 1000);
-  let hours = now.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  return `${hours}:${minutes}`;
-}
-function formatForecast(daystamp) {
-  let forecastDay = new Date(daystamp * 1000);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  return `${days[forecastDay.getDay()]}`;
-}
-
-function formatDate(daystamp) {
-  let today = new Date(daystamp * 1000);
-  let year = today.getFullYear();
-  let day = today.getDate();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  let weekday = days[today.getDay()];
-  let months = [
-    "Jan",
-    "Feb",
-    "March",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  let month = months[today.getMonth()];
-
-  return `${weekday}, ${month}. ${day}, ${year}`;
-}
-
-function getForecast(coordinates) {
-  let apiKey = "242e181ca0a34d6a4t3befc66o8e43fa";
-  let unit = "metric";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(displayForecast);
-}
 
 function displayResults(response) {
   celsiusTemperature = response.data.temperature.current;
@@ -79,19 +28,23 @@ function displayResults(response) {
   document.querySelector("#time").innerHTML = formatTime(response.data.time);
   document.querySelector("#date").innerHTML = formatDate(response.data.time);
 
-  getForecast(response.data.coordinates);
-}
+  function getForecast(coordinates) {
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(displayForecast);
+  }
 
-function displayForecast(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index) {
-      forecastHTML =
-        forecastHTML +
-        `<div class="col" id="forecast">
-              ${formatForecast(forecastDay.time)} <br />
+  function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index) {
+        forecastHTML =
+          forecastHTML +
+          `<div class="col" id="forecast">
+           <span class="forecast-day">${formatForecast(
+             forecastDay.time
+           )}</span><br />
             <img src="${
               forecastDay.condition.icon_url
             }" alt="" id="icon-forecast" />
@@ -103,15 +56,56 @@ function displayForecast(response) {
               forecastDay.temperature.minimum
             )} °</span>
             </div>`;
+      }
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+
+  function formatDate(daystamp) {
+    let today = new Date(daystamp * 1000);
+    let year = today.getFullYear();
+    let day = today.getDate();
+    let weekday = days[today.getDay()];
+    let months = [
+      "Jan",
+      "Feb",
+      "March",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    let month = months[today.getMonth()];
+
+    return `${weekday}, ${month}. ${day}, ${year}`;
+  }
+  function formatForecast(daystamp) {
+    let forecastDay = new Date(daystamp * 1000);
+    return `${days[forecastDay.getDay()]}`;
+  }
+  function formatTime(timestamp) {
+    let now = new Date(timestamp * 1000);
+    let hours = now.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
     }
-  });
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
+    let minutes = now.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+  }
+
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
-  let unit = "metric";
-  let apiKey = "242e181ca0a34d6a4t3befc66o8e43fa";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(displayResults);
 }
@@ -122,26 +116,17 @@ function handleSubmit(event) {
   search(city);
 }
 
-let searchEngine = document.querySelector("#search-engine");
-searchEngine.addEventListener("submit", handleSubmit);
-
-function showPosition(position) {
-  let unit = "metric";
-  let longitude = position.coords.longitude;
-  let latitude = position.coords.latitude;
-  let apiKey = "242e181ca0a34d6a4t3befc66o8e43fa";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&unit=${unit}`;
-  axios.get(apiUrl).then(displayResults);
+function conversionCelsius(event) {
+  event.preventDefault();
+  convertFahrenheit.classList.remove("active");
+  convertCelsius.classList.add("active");
+  let temperatureElement = document.querySelector(".degrees");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
 }
-
-let locationButton = document.querySelector(".current-location");
-locationButton.addEventListener("click", getCurrentLocation);
-
 function conversionFahrenheit(event) {
   event.preventDefault();
   convertFahrenheit.classList.add("active");
@@ -150,20 +135,25 @@ function conversionFahrenheit(event) {
   let temperatureElement = document.querySelector(".degrees");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
-
-function conversionCelsius(event) {
-  event.preventDefault();
-  convertFahrenheit.classList.remove("active");
-  convertCelsius.classList.add("active");
-  let temperatureElement = document.querySelector(".degrees");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+function showPosition(position) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&unit=${unit}`;
+  axios.get(apiUrl).then(displayResults);
 }
-let convertFahrenheit = document.querySelector(".fahrenheit");
-convertFahrenheit.addEventListener("click", conversionFahrenheit);
-
 let convertCelsius = document.querySelector(".celsius");
 convertCelsius.addEventListener("click", conversionCelsius);
 
+let convertFahrenheit = document.querySelector(".fahrenheit");
+convertFahrenheit.addEventListener("click", conversionFahrenheit);
+
+let locationButton = document.querySelector(".current-location");
+locationButton.addEventListener("click", getCurrentLocation);
+
+let searchEngine = document.querySelector("#search-engine");
+searchEngine.addEventListener("submit", handleSubmit);
+
+let apiKey = "242e181ca0a34d6a4t3befc66o8e43fa";
 let celsiusTemperature = null;
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+let unit = "metric";
 
 search("North Pole");
